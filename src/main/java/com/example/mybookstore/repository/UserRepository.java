07bfcd617533f7262ad9dfcebd7e21881a096f6e
Konstantin Umanets <konstantin@osh4.com>
2022-11-27 20:35:32 +0400

    Feature-3 Clean up the repository

--- src/main/java/com/example/mybookstore/repository/UserRepository.java
+++ src/main/java/com/example/mybookstore/repository/UserRepository.java
@@ -1,11 +1,7 @@
 package com.example.mybookstore.repository;
 
 import com.example.mybookstore.model.BookStoreUser;
-import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Mono;
 
 public interface UserRepository extends ReactiveCrudRepository<BookStoreUser, String> {
-    @Query("SELECT * FROM book_store_user WHERE LOWER(email) = LOWER(:username)")
-    Mono<BookStoreUser> findByUsername(String username);
 }
