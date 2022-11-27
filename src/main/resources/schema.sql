0b94e0fc964c919b9dbe40e4a132a6c93afd0a84
Konstantin Umanets <konstantin@osh4.com>
2022-11-27 13:59:23 +0400

    Feature-1 Add example schema and data

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,43 @@
+-- CREATE DATABASE "reactive-book-store";
+set schema 'public';
+
+DROP TABLE IF EXISTS book_store_user;
+
+CREATE TABLE book_store_user
+(
+    email      varchar(64) PRIMARY KEY,
+    name       varchar(64),
+    password   varchar(64),
+    roles      varchar(64)[],
+    birth_date date,
+    type       varchar(64)
+);
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE books
+(
+    title           varchar(200) PRIMARY KEY,
+    description     varchar(255),
+    publishing_date date,
+    price           decimal(10, 2),
+    author_email    varchar(64)
+);
+DROP TABLE IF EXISTS roles;
+
+CREATE TABLE roles
+(
+    name        varchar(200) PRIMARY KEY,
+    user_emails varchar(64)[]
+);
+
+INSERT INTO book_store_user(email, name, password, roles, birth_date, type)
+VALUES ('admin@test.com', 'Admin', '$2a$10$dXJ3SW6G7P50lGmMkkmwe.7ePbCSemAZgwYRI2srGPAeE24kMEyqK',
+        ARRAY ['ROLE_ADMIN'], null, 'ADMIN'),
+       ('author1@test.com', 'Author1', '$2a$10$dXJ3SW6G7P50lGmMkkmwe.7ePbCSemAZgwYRI2srGPAeE24kMEyqK',
+        ARRAY ['ROLE_AUTHOR'], '2000-01-01', 'AUTHOR'),
+       ('author2@test.com', 'Author2', '$2a$10$dXJ3SW6G7P50lGmMkkmwe.7ePbCSemAZgwYRI2srGPAeE24kMEyqK',
+        ARRAY ['ROLE_AUTHOR'], '2010-01-01', 'AUTHOR');
+
+INSERT INTO books(title, description, price, publishing_date, author_email)
+VALUES ('Test book 1', 'Amazing 1st test book', 12.2, '2010-01-01', 'author1@test.com'),
+       ('Test book 2', 'Boring 2nd test book', 34.4, '2000-01-01', 'author2@test.com')
